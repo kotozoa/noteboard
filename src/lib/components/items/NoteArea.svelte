5b7885d0140d5a117743ff4ca5c7b25b49d5eebc
vowopp <ballz869lol@gmail.com>
2026-08-12 12:01:26 -0400

    Create NoteArea.svelte

---
+++ src/lib/components/items/NoteArea.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Notepad from '$lib/components/items/Notepad.svelte';
+
+    let { notes } = $props();
+</script>
+
+<div class="notepad-container">
+    {#each notes as note}
+        <Notepad
+            x={note.x}
+            y={note.y}
+            size={note.size}
+            placeholder={note.placeholder}
+            bgcolor={note.bgcolor}
+        />
+    {/each}
+</div>
+
+<style>
+    .notepad-container {
+        background-color: black;
+        width: 90vw;
+        height: 90vh;
+    }
+</style>
